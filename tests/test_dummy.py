23be710f7f121546d3ed6363284df503a8e424c7
tom65536 <peregrinus@planet.ms>
2024-05-04 22:25:30 +0200

    style: code analysis

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -5,8 +5,8 @@ from __future__ import annotations
 
 def test_it() -> None:
     """Nonsense."""
-    a = 4
+    expected = 4
 
     result = 1 * 4
 
-    assert a == result
+    assert expected == result
